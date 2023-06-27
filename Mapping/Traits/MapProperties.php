5c5f2ac86f7facd3e7556c7fd2862fff936d6edc
n.zeinali <n.zeinali@epishkhan.ir>
2023-06-27 19:33:37 +0330

    change a comment and remove a comment code

--- Mapping/Traits/MapProperties.php
+++ Mapping/Traits/MapProperties.php
@@ -18,9 +18,8 @@ trait MapProperties
      */
     private function mapProperties(EntityInterface $entity, mixed $data, String $type): EntityInterface
     {
-        //Parse YAML file and get json mapper from it
+        //Parse YAML file and get relations mapping from it
         $properties = Yaml::parseFile(__DIR__.'/../Config/api.yaml')[$type];
-        //$properties = app(GetPropertiesReflector::class)->getClassPropertiesByReflection($entity);
         foreach ($properties as $proName => $proEqual) {
             $proEqualValue = $this->getPropertyValue($proEqual, $data);
             $this->propertyAccessor->setValue($entity, $proName, $proEqualValue);
